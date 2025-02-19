eb2ecbfe007f3b2e0c17fb5613933da23c9871a8
Victor-Johnson <victorjohnson_979@yahoo.com>
2025-02-19 16:31:49 +0100

    Added Docker, Github Actions and List API for news

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,22 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   webpack: (config) => {
+//     config.module.rules.push({
+//       test: /\.svg$/i,
+//       issuer: /\.[jt]sx?$/,
+//       use: ["@svgr/webpack"],
+//     });
+//     return config;
+//   },
+//   images: {
+//     domains: ["res.cloudinary.com"], // Add Cloudinary domain here
+//   },
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -11,7 +30,10 @@ const nextConfig: NextConfig = {
     return config;
   },
   images: {
-    domains: ["res.cloudinary.com"], // Add Cloudinary domain here
+    domains: ["res.cloudinary.com"], 
+  },
+  eslint: {
+    ignoreDuringBuilds: true, 
   },
 };
 
