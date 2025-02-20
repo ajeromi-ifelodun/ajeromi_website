78c96d0ff0faf1c135f537d3dbe73a3cbaad4e44
jokings2k <eromoselejonthan@gmail.com>
2025-02-20 21:17:46 +0100

    added the neew endpoints

--- app/_page/latest.tsx
+++ app/_page/latest.tsx
@@ -11,9 +11,10 @@ import news4 from "../../public/static/image/news4.png";
 import { getRequest } from "../queries/requests";
 import { useQuery } from "@tanstack/react-query";
 import { NewsArticle } from "../../helpers/types";
+import { Skeleton } from "@nextui-org/react";
 const Latest = () => {
   const { data, isFetching, isLoading, isError, isSuccess } = useQuery({
-    queryKey: ["get_publications"],
+    queryKey: ["get_newslist"],
     queryFn: () => getRequest("/newslist"),
   });
   console.log(data)
@@ -27,13 +28,29 @@ const Latest = () => {
           <p className="text-pjgreen text-[1.5rem] ">see all</p> <Rightarrow />
         </Link>
       </div>
-      <div className="grid grid-cols-1 gap-10 justify-items-center row-span-9 justify-center sm:grid-cols-2 md:grid md:grid-cols-3 lg:flex lg:justify-between mt-10">
-        {result?.slice(0,4)?.map((res,index) => (
-          <Latestcards img={res?.url} date="Nov 29, 2024" title={res.title} paragraph={res.description} key={index}/>
-        ))}
-
-        
-      </div>
+      <Skeleton
+        isLoaded={data !== null}
+        className={`${
+          data == null && "h-[300px]"
+        }`}
+      >
+        <div className="grid grid-cols-1 gap-10 justify-items-center row-span-9 justify-center sm:grid-cols-2 md:grid md:grid-cols-3 lg:flex lg:justify-between mt-10">
+          {/* <Skeleton
+          className="w-full grid grid-cols-1 gap-10 justify-items-center row-span-9 justify-center sm:grid-cols-2 md:grid md:grid-cols-3 lg:flex lg:justify-between mt-10"
+          isLoaded={isSuccess}
+        > */}
+          {result?.slice(0, 4)?.map((res, index) => (
+            <Latestcards
+              img={res?.url}
+              date="Nov 29, 2024"
+              title={res.title}
+              paragraph={res.description}
+              key={index}
+            />
+          ))}
+          {/* </Skeleton> */}
+        </div>
+      </Skeleton>
     </div>
   );
 };
--- app/queries/requests.ts
+++ app/queries/requests.ts
@@ -6,6 +6,8 @@ export const getRequest = async (url: string) => {
     const response = await axiosInstance.get(url);
     if (response.data?.status) {
       return SuccessResponse(response?.data?.data, response?.data?.message);
+    } else if(response.status == 200){
+      return SuccessResponse(response?.data, response?.data?.message);
     } else {
       return FailedResponse(
         response?.data?.message || "Some error occured",
@@ -24,6 +26,7 @@ export const getRequest = async (url: string) => {
 export const postRequest = async (data: any, url: string) => {
   try {
     const response = await axiosInstance.post(url, data);
+    console.log(response)
     if (response.data?.status) {
       return SuccessResponse(response?.data?.data, response?.data?.message);
     } else {
