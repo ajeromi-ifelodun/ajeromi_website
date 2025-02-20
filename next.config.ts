6af4d278a6ba8f8e5c269af644e6c1ae0be92f0b
Victor-Johnson <victorjohnson_979@yahoo.com>
2025-02-20 17:50:17 +0100

    Tweak to CORS in Next config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,8 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   webpack: (config) => {
-//     config.module.rules.push({
-//       test: /\.svg$/i,
-//       issuer: /\.[jt]sx?$/,
-//       use: ["@svgr/webpack"],
-//     });
-//     return config;
-//   },
-//   images: {
-//     domains: ["res.cloudinary.com"], // Add Cloudinary domain here
-//   },
-// };
-
-// export default nextConfig;
-
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
@@ -30,10 +12,33 @@ const nextConfig: NextConfig = {
     return config;
   },
   images: {
-    domains: ["res.cloudinary.com"], 
+    domains: ["res.cloudinary.com"],
   },
   eslint: {
-    ignoreDuringBuilds: true, 
+    ignoreDuringBuilds: true,
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: isProduction
+              ? "http://109.176.199.133:3000"  // Production URL
+              : "http://localhost:3000",      // Development URL
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
   },
 };
 
