9b89e4d7622abd0fa7764eb764904989c5e76d78
jokings2k <eromoselejonthan@gmail.com>
2025-02-22 09:05:05 +0100

    added the new pages

--- app/aboutus/_page/visionstat.tsx
+++ app/aboutus/_page/visionstat.tsx
@@ -11,13 +11,12 @@ const VisionStat = () => {
       </div>
       <div className="text-[#FFFFFF] w-full md:w-2/3  flex flex-col gap-5 justify-center">
         <h3 className="sectionheader text-center md:text-end ">
-          Mission Statement
+          vision Statement
         </h3>
         <p className="text-center md:text-end">
-          To serve as a leading institution in the grassroots, focusing on
-          providing infrastructure, quality education, economic progression,
-          effective healthcare delivery, security, and improved welfare for the
-          masses.
+          To ensure a legacy of a government that is responsive to the
+          socio-economic dynamics of transforming the lifestyles of the
+          grassroots.
         </p>
       </div>
     </div>
